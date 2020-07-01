163ce82472d51665f1532060304557ee15197032
alishechka <alidrake01@gmail.com>
2020-07-01 19:31:05 +0100

    first commit

---
+++ app/src/main/java/com/example/firebasemessengerapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.firebasemessengerapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("Main Activity", "email is " + email)
+            Log.d("Main Activity", "password is $password")
+        }
+
+        already_have_account_text_view.setOnClickListener {
+            Log.d("Main Activity", "show login activity")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
